f110a741c57ed80be975a34fc97e664b575abe72
Sreedevi <sreedevisobhana@gmail.com>
2022-08-15 17:33:12 +0530

    homescreen and folder str

---
+++ blog_app/src/Screens/AfterLogin/EditScreen/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>ditScreen</Text>
+    </View>
+  )
+}
+
+export default EditScreen
+
+const styles = StyleSheet.create({})
---
+++ blog_app/src/Screens/AfterLogin/HomeScreen/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+container:{
+    flex:1, 
+    backgroundColor:'white'
+},
+title:{
+    color: 'black', 
+    fontSize: 18,
+     fontWeight: 'bold'
+},
+name:{
+ color:'black',
+ fontStyle:'italic',
+ fontSize:10,
+}, 
+comments:{
+color:'blue'
+}
+})
+
+export {styles}
