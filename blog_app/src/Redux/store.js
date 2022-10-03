7097f822957ef3128b21f0d001e1c13f318c4986
Sreedevi <sreedevisobhana@gmail.com>
2022-10-03 07:39:45 +0530

    used redux persist

--- blog_app/src/Redux/store.js
+++ blog_app/src/Redux/store.js
@@ -1,7 +1,15 @@
 import {applyMiddleware, createStore, compose} from 'redux'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import appReducer from './Reducers'
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+  }
+  
 
 var composeEnhancers = compose
 if(__DEV__){
@@ -9,5 +17,10 @@ if(__DEV__){
 }
 
 
-const store = createStore(appReducer, composeEnhancers(applyMiddleware(thunk)))
+const persistedReducer = persistReducer(persistConfig, appReducer)
+
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
+export let persistor = persistStore(store)
+
+
 export default store
